e037570107c9b3f3056168c5295595eef117ee01
pale3 <marko.rakamaric@gmail.com>
2014-09-07 16:49:08 +0200

    replaced echo with info.

--- lib/savedconfig.bash
+++ lib/savedconfig.bash
@@ -6,17 +6,17 @@ restore_config(){
 	 # as makepkg -o needs to be executed first
 	 if [[ ! -d "${srcdir}/${pkgname}/" ]]; then
 		 MODIFY=0
-		 echo "There is no "${srcdir}/${pkgname}/", you need to run  makepkg -o first!!"
+		 info -r "There is no "${srcdir}/${pkgname}/", you need to run  makepkg -o first!!"
 		 clean
-		 exit 1
+		 exit 255
 	 else
 		cp ${CONFIGDIR}/${pkgname}/${config} "${srcdir}/${pkgname}/"
 	 fi
 
  else
-	 echo "file ${CONFIGDIR}/${pkgname}/${config} doesn't exist" 
-	 echo "cp your config file to ${CONFIGDIR}/${pkgname}/ for later use"
-	 echo "then create line config ${config} in ${pkgname}.cstpkg"
+	 info -e "file ${CONFIGDIR}/${pkgname}/${config} doesn't exist" 
+	 info -m "cp your config file to ${CONFIGDIR}/${pkgname}/ for later use"
+	 info -m "then create line config ${config} in ${pkgname}.cstpkg"
  fi
  return 0
 }
