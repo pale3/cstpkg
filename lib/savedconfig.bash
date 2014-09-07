6e2e2817f5c541327762ab41e7a3860c6977ee1d
pale3 <marko.rakamaric@gmail.com>
2014-09-07 22:19:19 +0200

    add mising comments on examples, Fixed fallback operand

--- lib/savedconfig.bash
+++ lib/savedconfig.bash
@@ -1,22 +1,22 @@
 restore_config(){
 	local config="${1}" srcdir="$(pwd)/src"
 
-	if [[ -f ${CONFIGDIR}/${pkgname}/${config} ]]; then
+	if [[ -f ${CONFIGDIR}/${package}/${config} ]]; then
 	 # what if srcdir/pkgname doesn't exist than we can't operate	
 	 # as makepkg -o needs to be executed first
-	 if [[ ! -d "${srcdir}/${pkgname}/" ]]; then
+	 if [[ ! -d "${srcdir}/${package}/" ]]; then
 		 MODIFY=0
-		 info -e "There is no "${srcdir}/${pkgname}/", you need to run  makepkg -o first!!"
+		 info -e "There is no "${srcdir}/${package}/",\n     you need to run  makepkg -o first!!"
 		 clean
 		 exit 255
 	 else
-		cp ${CONFIGDIR}/${pkgname}/${config} "${srcdir}/${pkgname}/"
+		 cp "${CONFIGDIR}/${package}/${config}" "${srcdir}/${package}/"
 	 fi
 
  else
-	 info -e "file ${CONFIGDIR}/${pkgname}/${config} doesn't exist" 
-	 info -m "cp your config file to ${CONFIGDIR}/${pkgname}/ for later use"
-	 info -m "then create line config ${config} in ${pkgname}.cstpkg"
+	 info -e "file ${CONFIGDIR}/${package}/${config} doesn't exist" 
+	 info -m "cp your config file to ${CONFIGDIR}/${package}/ for later use"
+	 info -m "then create line config ${config} in ${package}.cstpkg"
  fi
  return 0
 }
