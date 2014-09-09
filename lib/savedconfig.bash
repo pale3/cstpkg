008f2d31a39912fa5428eb91c689a9ee96b6aa1a
pale3 <marko.rakamaric@gmail.com>
2014-09-09 15:00:13 +0200

    better usage of savedconfig, modified for error handling

--- lib/savedconfig.bash
+++ lib/savedconfig.bash
@@ -1,22 +1,22 @@
 restore_config(){
-	local config="${1}" srcdir="$(pwd)/src"
+	local config="${1}" startdir="$(pwd)" srcdir="$(pwd)/src"
 
 	if [[ -f ${CONFIGDIR}/${package}/${config} ]]; then
-	 # what if srcdir/pkgname doesn't exist than we can't operate	
-	 # as makepkg -o needs to be executed first
-	 if [[ ! -d "${srcdir}/${package}/" ]]; then
-		 MODIFY=0
-		 info -e "There is no "${srcdir}/${package}/",\n     you need to run  makepkg -o first!!"
-		 clean
-		 exit 255
-	 else
-		 cp "${CONFIGDIR}/${package}/${config}" "${srcdir}/${package}/"
-	 fi
+		 info -m "Copying '$config' to '$startdir'"
+		 
+		 # copy configuration from /etc to startdir
+		 cp "${CONFIGDIR}/${package}/${config}" "./"
 
+		 # create src dir if it doesn't exist	
+		 mkdir -p src
+		 
+		 # link configuration to src dir for later use
+		 [[ ! -f ${srcdir}/${config} ]] && ln -s "$(pwd)/${config}" "src/"
+		 return 0
  else
 	 info -e "file ${CONFIGDIR}/${package}/${config} doesn't exist" 
 	 info -m "cp your config file to ${CONFIGDIR}/${package}/ for later use"
 	 info -m "then create line config ${config} in ${package}.cstpkg"
+	 return 127
  fi
- return 0
 }
