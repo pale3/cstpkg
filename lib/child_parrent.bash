a65a71b7c308bd39f3785649968cdf38bd129663
pale3 <marko.rakamaric@gmail.com>
2014-09-09 17:50:37 +0200

    added child_parrent lib, Determine if %update-vars need to be executed

---
+++ lib/child_parrent.bash
@@ -0,0 +1,15 @@
+is_child_of(){
+	#local invokedby=$(pstree | grep customizepkg | awk -F'---' '{print $2}')
+	local	invokedby=$(ps -ocommand= -p $PPID | awk '{print $2}' | awk -F/ '{print $NF}')
+	echo $invokedby
+}
+
+is_parent_or_child(){
+	local process=$(ps -o stat= -p $PPID)
+	
+	case $process in
+		S+ ) echo child ;;
+	  Ss ) echo parrent ;;
+	esac 
+
+}
